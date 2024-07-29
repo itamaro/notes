6dfb64bd7684883cd59bec09d449011cea29701b
Itamar Oren <itamarost@gmail.com>
2024-07-29 11:34:16 -0700

    Update base URL for custom domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     //   provider: "plausible",
     // },
     locale: "en-US",
-    baseUrl: "itamaro.github.io/notes",
+    baseUrl: "itamaro.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
